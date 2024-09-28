f8983d408611985d90eb957814877fa34f47d95c
llammann <bd7n2v9tr.code.edu.az>
2024-09-29 01:44:47 +0400

    Let's build a Teacher interface

---
+++ TypeScript/task_1/js/main.ts
@@ -0,0 +1,19 @@
+//Teacher interface with index signature
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [propName: string]: any;
+}
+
+const teacher3: Teacher = {
+  firstName: 'John',
+  lastName: 'Doe',
+  fullTimeEmployee: false,
+  location: 'London',
+  contract: false,
+};
+
+console.log(teacher3);
